ec72b12f3b1be208e7c31ddeed890fecbc3557f2
bayo96 <bababacar529@gmail.com>
2026-05-19 16:17:45 +0000

    frontendpages

---
+++ frontend/src/pages/accueil.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pages.css'; // Assurez-vous que le chemin est correct
+const Accueil = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur notre site</h1>
+        <img src="path/to/your/image.jpg" alt="Description de l'image" />
+    </div>
+  )
+}
+
+export default Accueil
